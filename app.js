19ebf7e340f9d820ea8d8ed0c34a90d9d4a5a597
uriel luqui <urielluqui@gmail.com>
2024-08-12 00:41:25 -0300

    arreglos

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const products = [
-    { id: 1, name: "Producto A", price: 150, description: "Producto A Descripción", image: "assets/negra-21-b1c72bbcebc7feb9b816405376283015-1024-1024.jpg" },
-    { id: 2, name: "Producto B", price: 200, description: "Producto B Descripción", image: "assets/remera-adulto-unisex-blanca-sublimable.jpg" },
-    { id: 3, name: "Producto C", price: 250, description: "Producto C Descripción", image: "" }
+    { id: 1, name: "Remera Negra", price: 15000, description: "Producto A Descripción", image: "/assets/negra-21-b1c72bbcebc7feb9b816405376283015-1024-1024.jpg" },
+    { id: 2, name: "Remera Blanca", price: 20000, description: "Producto B Descripción", image: "/assets/remera-adulto-unisex-blanca-sublimable.jpg" },
 ];
 
 const filteredProducts = products.filter(product => product.price > 150);
@@ -16,11 +15,9 @@ function displayProducts(productList) {
         const card = document.createElement('div');
         card.classList.add('card');
 
-        const imgSrc = product.image;
-        console.log('Ruta de imagen generada:', imgSrc);
 
         card.innerHTML = `
-        <img src="${imgSrc}" alt="${product.name}">
+        <img src="${product.image}" alt="${product.name}">
             <h2>${product.name}</h2>
             <p>${product.description}</p>
             <p>Precio: $${product.price}</p>
@@ -66,3 +63,5 @@ function loadExternalData() {
 document.addEventListener('DOMContentLoaded', () => {
     loadLocalData();
 });
+
+
